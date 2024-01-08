cfe6f531dc10006ae214baaf0dfbfefe24e757b5
Shenggao Li <qluanl.sg@gmail.com>
2024-01-08 13:12:00 +0000

    Change to FastPWM for higher resolution

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 #include "EthernetInterface.h"
 #include "ExperimentServer.h"
 #include "QEI.h"
+#include "FastPWM.h"
 #define PI 3.14159265358979323846
+#define PWMfrequency 5000
+#define PWMperiodTicks 12000 // == 60 000 000 / PWMfrequency
+
+// Trick to bypass ISR error
+class MyAnalogIn : public AnalogIn {
+public:
+    MyAnalogIn(PinName inp) : AnalogIn(inp) { }
+    virtual void lock() { }
+    virtual void unlock() { }
+};
 
 // Define number of communication parameters with matlab
 #define NUM_INPUTS 2
@@ -17,10 +28,10 @@ DigitalOut led_g(LED_GREEN,1);      // UDP server state indicator
 
 /************************Complete the code in this block**************************/
 // Assign digital/analog pins for control and sensing
-PwmOut     M1PWM(D9);              // Motor PWM output
+FastPWM    M1PWM(D9);               // Motor PWM output (we are using the "FastPWM" rather than built-in "PwmOut" for higher resolution)
 DigitalOut M1INA(D2);               // Motor forward enable
 DigitalOut M1INB(D4);               // Motor backward enable
-AnalogIn   CS(A2);                  // Current sensor
+MyAnalogIn   CS(A2);                // Current sensor
 /*********************************************************************************/
 
 // Create a quadrature encoder
@@ -31,10 +42,10 @@ QEI encoder(D5,D3, NC, 1200 , QEI::X4_ENCODING);
 const float radPerTick = 2.0*PI/1200.0;
 
 // Set motor duty [-1.0f, 1.0f]
-void setMotorDuty(float duty, DigitalOut &INA, DigitalOut &INB, PwmOut &PWM);
+void setMotorDuty(float duty, DigitalOut &INA, DigitalOut &INB, FastPWM &PWM);
 
 const float SupplyVoltage = 12;     // Supply voltage in Volts
-void setMotorVoltage(float voltage, DigitalOut &INA, DigitalOut &INB, PwmOut &PWM);
+void setMotorVoltage(float voltage, DigitalOut &INA, DigitalOut &INB, FastPWM &PWM);
 
 int main (void) {
     // Link the terminal with our server and start it up
@@ -42,8 +53,9 @@ int main (void) {
     server.init();
     led_g = 0;  // UDP server is ready, turn on green led
 
-    // PWM period should nominally be a multiple of our control loop
-    M1PWM.period_us(50);
+    // Setting PWM frequency. Here is 5k Hz, same as the current loop
+    M1PWM.prescaler(1);
+    M1PWM.period_ticks(PWMperiodTicks);
     
     // Continually get input from MATLAB and run experiments
     float input_params[NUM_INPUTS];
@@ -93,12 +105,12 @@ int main (void) {
 } // end main
 
 //Set motor voltage (nagetive means reverse)
-void setMotorVoltage(float voltage, DigitalOut &INA, DigitalOut &INB, PwmOut &PWM){
+void setMotorVoltage(float voltage, DigitalOut &INA, DigitalOut &INB, FastPWM &PWM){
     setMotorDuty(voltage / SupplyVoltage, INA, INB, PWM);
 }
 
 // Set motor duty [-1.0f, 1.0f]
-void setMotorDuty(float duty, DigitalOut &INA, DigitalOut &INB, PwmOut &PWM)
+void setMotorDuty(float duty, DigitalOut &INA, DigitalOut &INB, FastPWM &PWM)
 {
     unsigned char reverse = 0;
 
@@ -118,5 +130,9 @@ void setMotorDuty(float duty, DigitalOut &INA, DigitalOut &INB, PwmOut &PWM)
         INB = 0;
     }
 
-    PWM.write(duty);
+    // The standard way to set built-in PWM is: 
+    // PWM.write(duty);
+    // However, we are using a high resolution PWM (FastPWM library). 
+    // We have to set the PWM is the following way.
+    PWM.pulsewidth_ticks((int) (PWMperiodTicks*duty));
 }
