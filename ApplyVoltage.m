e0cf2bb1fb8f523999a828aee6941012a14b9108
Shenggao Li <qluanl.sg@gmail.com>
2024-02-04 14:48:38 -0500

    Fine tune Matlab script

--- ApplyVoltage.m
+++ ApplyVoltage.m
@@ -1,4 +1,7 @@
 function output_data = ApplyVoltage(voltage)
+%ApplyVoltage Apply voltage on the DC motor
+%   Takes the command voltage as input
+%   If no voltage is given, will use default: 3V
     if nargin == 0
         % Set voltage to 3V if run directly
         voltage = 3;
@@ -28,32 +31,35 @@ function output_data = ApplyVoltage(voltage)
     % This function will get called any time there is new data from
     % the FRDM board. Data comes in blocks, rather than one at a time.
     function my_callback(new_data)
-        t = new_data(:,1);   % time
-        angle = new_data(:,2); % position
-        vel = new_data(:,3); % velocity
-        voltage = new_data(:,4); % voltage
+        t       = new_data(:,1); % time
+        angle   = new_data(:,2); % position
+        vel     = new_data(:,3); % velocity
+        volt    = new_data(:,4); % voltage
         current = new_data(:,5); % current
-        N = length(angle);
+        N       = length(angle); % number of data points
         
         h1.XData(end+1:end+N) = t;   % Update subplot 1
         h1.YData(end+1:end+N) = angle;
         h2.XData(end+1:end+N) = t;   % Update subplot 2
         h2.YData(end+1:end+N) = vel;
         h3.XData(end+1:end+N) = t;   % Update subplot 3
-        h3.YData(end+1:end+N) = voltage;
+        h3.YData(end+1:end+N) = volt;
         h4.XData(end+1:end+N) = t;   % Update subplot 4
         h4.YData(end+1:end+N) = current;
     end
     
+    % Setup the communication between PC and FRDM board
     frdm_ip  = '192.168.1.100';     % FRDM board ip
     frdm_port= 11223;               % FRDM board port  
     params.callback = @my_callback; % callback function
     params.timeout  = 2;            % end of experiment timeout
 
-    ExperimentTime = 5;             % in seconds
     % Pack experiment parameters
+    ExperimentTime = 5;             % in seconds
     input = [voltage, ExperimentTime];
-    output_size = 5;                % time, current, angle
+
+    % Number of report data from FRDM
+    output_size = 5;                % time, angle, veloticy, voltage, current
    
     output_data = RunExperiment(frdm_ip,frdm_port,input,output_size,params);
     
